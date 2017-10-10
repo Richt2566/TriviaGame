7e4c6716b7255ac32448a0c90fb06ad293d8f513
Rich T <rtranter25@gmail.com>
2017-10-10 09:41:27 -0500

    finished for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,6 @@ var clockRunning = false;
 
 var intervalId;
 
-
 var stopwatch = {
 	time: 30,
 
@@ -21,16 +20,12 @@ var stopwatch = {
 
 		$("#display").html("00:30");
     outOfTime();
+    $("#questionarea").addClass("show");
 
 		if (!clockRunning) {
         intervalId = setInterval(stopwatch.count, 1000);
       	
       	clockRunning = true;
-
-      //   if (?? < 1) {
-      //     $("#questionarea").html("<div> You ran out of time...</div>")
-      //   }
-
     }
 	},
 
@@ -40,20 +35,17 @@ var stopwatch = {
 		var converted = stopwatch.timeConverter(stopwatch.time);
 
     	$("#display").html(converted);
-
-      // if (count === 0) {
-      //   $("#questionarea").html("<div> You ran out of time...</div>")
-      // }
 	},
 
   stop: function() {
     clearInterval(intervalId);
     clockRunning = false;
+    stopTime();
+
   },
 
 	timeConverter: function(t) {
 
-    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
 
@@ -74,98 +66,22 @@ var stopwatch = {
 
 }
 
-// var myVar; 
+var myVar;
 
 function outOfTime() {
 
-  setTimeout(what, 30000);
+myVar = setTimeout(what, 30000);
 
+}
+
+function stopTime() {
+  clearTimeout(myVar);
 }
 
 function what() {
   $("#display").remove();
-  // $("#questionarea").html("<div> You ran out of time...</div>");
-  
-  // if ($("input[name='q1']:checked").val() &&
-  //     $("input[name='q2']:checked").val() &&
-  //     $("input[name='q3']:checked").val() &&
-  //     $("input[name='q4']:checked").val()) {
-  //   clearTimeout(myVar);
-  //   console.log("it works");
-  // }
-  $("#questionarea").html("<div> The correct Answers are:<br> 1: 'a' Natalie Portman<br> 2: 'b' Fight Club<br> 3: 'b' Padmé Amidala<br> 4: 'c' Christopher Lee<br></div>");
-  // $("#outcome").html("<p>Your answers are:<br> 1: " + $("input[name='q1']:checked").val()+ 
-  //     "<br>2: " + $("input[name='q2']:checked").val() + "<br> 3: " + $("input[name='q3']:checked").val() + 
-  //     "<br> 4: " + $("input[name='q4']:checked").val() + "</p>");
-  $("#outcome").html("<p> Correct answers: " + rightanswer + "</br> Incorrect answers: " + wronganswer + "<br></p>")
-
-}
-
-//event target closest input radio button (button)
-
-// var count = 12;
-
-// var seeCount;
-
-// function timeWords() {
-// 	$("#comments").html("<p></p>");
-// }
-
-// function countDown() {
-// 	count--
-// 	seeCount = setInterval(displayCount, 1000);
-// 	if(count === 0) {
-// 		count = 12
-// 	}
-// }
-
-// function displayCount() {
-// 	$("#comments").html("<p>" + count + "</p>");
-// }
-
-
-//var timer = setTimeout(timeWords, 30000);
-
-function startTrivia() {
-	$("#questionarea").addClass("show");
-	console.log("show");
-}
-
-// function myCorrect() {
-// 	$("#outcome").html("<h2> That is correct</h2>");
-// 	console.log("correct");
-// }
-
-// function myIncorrect() {
-// 	$("#outcome").html("<h2> That is incorrect</h2>");
-// 	console.log("incorrect");
-// }
-
-//info for the questions
-
-
-// var ques1 = ($("input[name='q1']:checked").val() != "a");
-           
-// var ques2 = ($("input[name='q2']:checked").val() != "b");
-
-// var ques3 = ($("input[name='q3']:checked").val() != "b");
-
-// var ques4 = ($("input[name='q4']:checked").val() != "c");
-
-function displayAnswers() {
-	$("#questionarea").html("<div> The correct Answers are:<br> 1: 'a' Natalie Portman<br> 2: 'b' Fight Club<br> 3: 'b' Padmé Amidala<br> 4: 'c' Christopher Lee<br></div>");
-  // $("#outcome").html("<p>Your answers are:<br> 1: " + $("input[name='q1']:checked").val()+ 
-  //     "<br>2: " + $("input[name='q2']:checked").val() + "<br> 3: " + $("input[name='q3']:checked").val() + 
-  //     "<br> 4: " + $("input[name='q4']:checked").val() + "</p>");
-  $("#outcome").html("<p> Correct answers: " + rightanswer + "</br> Incorrect answers: " + wronganswer + "<br></p>")
-
-}
-
-var rightanswer = 0;
-var wronganswer = 0;
-
-function finished() {
-	$("#display").remove();
+  $("#startbutton").remove();
+  $("#timesup").html("<div> You ran out of time...</div>");
 
   if ($("input[name='q1']:checked").val() == "a") {
     rightanswer++
@@ -185,11 +101,36 @@ function finished() {
     wronganswer++
   }
 
-  if ($("input[name='q4']:checked").val() == "b") {
+  if ($("input[name='q4']:checked").val() == "c") {
     rightanswer++
   } else {
     wronganswer++
   }
+  
+  $("#questionarea").html("<div> The correct Answers are:<br> 1: 'a' Natalie Portman<br> 2: 'b' Fight Club<br> 3: 'b' Padmé Amidala<br> 4: 'c' Christopher Lee<br></div>");
+  $("#outcome").html("<p> Correct answers: " + rightanswer + "</br> Incorrect answers: " + wronganswer + "<br></p>")
+
+  $("#donebutton").remove();
+  $("#nextbutton").addClass("show");
+}
+
+function startTrivia() {
+	$("#questionarea").addClass("show");
+	console.log("show");
+}
+
+function displayAnswers() {
+	$("#questionarea").html("<div> The correct Answers are:<br> 1: 'a' Natalie Portman<br> 2: 'b' Fight Club<br> 3: 'b' Padmé Amidala<br> 4: 'c' Christopher Lee<br></div>");
+  $("#outcome").html("<p> Correct answers: " + rightanswer + "</br> Incorrect answers: " + wronganswer + "<br></p>")
+
+}
+
+var rightanswer = 0;
+var wronganswer = 0;
+
+function finished() {
+  clearTimeout(what);
+  stopTime();
 	
 	if (!$("input[name='q1']:checked").val() ||
       !$("input[name='q2']:checked").val() ||
@@ -198,8 +139,42 @@ function finished() {
       ) {
       alert("You're not done yet!");
   } else {
-  	displayAnswers();
+
+  if ($("input[name='q1']:checked").val() == "a") {
+    rightanswer++
+  } else {
+    wronganswer++
   }
+
+  if ($("input[name='q2']:checked").val() == "b") {
+    rightanswer++
+  } else {
+    wronganswer++
+  }
+
+  if ($("input[name='q3']:checked").val() == "b") {
+    rightanswer++
+  } else {
+    wronganswer++
+  }
+
+  if ($("input[name='q4']:checked").val() == "c") {
+    rightanswer++
+
+  } else {
+    wronganswer++
+  }
+
+    displayAnswers();
+    $("#display").remove();
+    $("#startbutton").remove();
+    $("#donebutton").remove();
+    $("#nextbutton").addClass("show");
+    }
+}
+
+function refreshPage(){
+    window.location.reload();
 }
 
 $(document).ready(function(){
@@ -207,5 +182,7 @@ $(document).ready(function(){
 	$("#donebutton").click(finished);
 
 	$("#startbutton").click(stopwatch.start);
+
+  $("#nextbutton").click(refreshPage);
 	
 });
